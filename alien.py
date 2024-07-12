3230a8fe0cfa80698712b4b76e4e5347479215c9
liquidsworrds <adithyasunil@pm.me>
2024-07-12 21:47:38 +0530

    adding alien

---
+++ alien.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    def __init__(self, ai_settings, screen):
+        super().__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        self.image = pygame.image.load('images/alien.png')
+        self.rect = self.image.get_rect()
+
+        # Generate new alien at the top left
+        self.rect.x = self.rect.width // 6
+        self.rect.y = self.rect.height // 6
+
+        # Store alien position
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
